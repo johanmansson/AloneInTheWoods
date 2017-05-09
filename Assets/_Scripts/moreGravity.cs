90c8f3bac057fb9add85e2102dcd9d31ded26c81
Johan Månsson <mansson.johan@me.com>
2017-05-09 11:04:25 +0200

    added gravity script on gameobjects

---
+++ Assets/_Scripts/moreGravity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moreGravity : MonoBehaviour {
+
+	public void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(Physics.gravity, ForceMode.Acceleration);
+    }
+
+}
